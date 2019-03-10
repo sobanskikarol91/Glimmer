926fe4765bbe653c3f89227d89e62cada595736a
Karol Sobanski <sobanskikarol91@gmail.com>
2019-03-11 00:32:05 +0100

    Add: InputHandler + sticking to the walls

---
+++ GTU/Assets/Scripts/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float force;
+
+    private Vector3 dragEnd, dragStart;
+    private Rigidbody2D rb;
+    private bool isGlued;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        InputHandler.OnTapEnd += TapEnd;
+        InputHandler.OnTapStart += TapStart;
+    }
+
+    void TapEnd()
+    {
+        dragEnd = GetMousePosition();
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        AddForce();
+    }
+
+    void TapStart()
+    {
+        dragStart = GetMousePosition();
+    }
+
+    void AddForce()
+    {
+        Vector3 direction = dragEnd - dragStart;
+        rb.AddForce(-direction * force, ForceMode2D.Impulse);
+    }
+
+    void GlueObject()
+    {
+        isGlued = true;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.velocity = Vector3.zero;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGlued = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!isGlued)
+        {
+            isGlued = true;
+            GlueObject();
+        }
+    }
+
+    Vector3 GetMousePosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
---
+++ GTU/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputHandler : MonoBehaviour
+{
+    public delegate void State();
+    public static State OnTapStart, OnTapEnd;
+
+
+    private void Update()
+    {
+        CheckTapStart();
+        CheckTapEnd();
+    }
+
+    private void CheckTapStart()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnTapStart();
+    }
+
+    private void CheckTapEnd()
+    {
+        if (Input.GetMouseButtonUp(0))
+            OnTapEnd();
+    }
+}
