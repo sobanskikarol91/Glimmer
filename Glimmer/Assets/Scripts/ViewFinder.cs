ec1c045cd60b604d0968f735058e516abb6a219d
Karol Sobanski <sobanskikarol91@gmail.com>
2019-03-10 23:54:06 +0100

    Initial Project

---
+++ Glimmer/Assets/Scripts/ViewFinder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewFinder : MonoBehaviour
+{
+    [SerializeField] Transform origin;
+    [SerializeField] float speed;
+    
+    public void Update()
+    {
+        transform.RotateAround(origin.position, new Vector3(0, 0, 1), Time.deltaTime * speed);
+    }
+}
+
+
